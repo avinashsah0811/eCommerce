b503587b09fbd0a4821a0bee2a94398b3fe6f247
avinashsah0811 <avinashsah0811@gmail.com>
2024-03-20 16:34:19 +0530

    routing don using react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { HomeScreen } from "./screens/HomeScreen";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
@@ -9,7 +11,8 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome To eCommerce</h1>
+          {/* <HomeScreen /> */}
+          <Outlet />
         </Container>
       </main>
       <Footer />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,27 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// for routing purposes
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
+import { HomeScreen } from './screens/HomeScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+    </Route>
+  )
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
