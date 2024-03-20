765a8ba2eb7281297752ecc1c75020dd6d496ff1
avinashsah0811 <avinashsah0811@gmail.com>
2024-03-20 22:33:20 +0530

    basic setup for backend done

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import products from './data/products.js';
+const PORT = 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+    console.log('products loaded');
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(p => p._id === req.params.id);
+    res.json(product);
+    console.log(`specific product with id: ${product._id} loaded`);
+})
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}!`);
+})
