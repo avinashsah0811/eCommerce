6ab0bd925a12203cbc8a6f5c9f37c58c613fa08e
avinashsah0811 <avinashsah0811@gmail.com>
2024-03-21 11:30:21 +0530

    database created, connection established with database

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.DATABASE_URL)
+        console.log(`Mongodb connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`error: ${error.message}`);
+        console.log(`error: ${error}`);
+    }
+}
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import products from './data/products.js';
+import connectDB from './config/db.js'
 const PORT = process.env.PORT || 5000;
+
+connectDB(); // connect to database
 
 const app = express();
 
